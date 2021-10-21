d5caadeca3c94b532f45996bde551f643822e6d4
Josip-github <josipcota1@gmail.com>
2021-10-21 14:53:32 +0200

    This commit creates a method uploadMultipleFiles() '/' in line 33 refers to '/' in line 21, which means after uploading files getPage() method will be called, in order to display the uploaded files.

--- src/main/java/com/example/uploadanddownloadfilesapp/controller/DocController.java
+++ src/main/java/com/example/uploadanddownloadfilesapp/controller/DocController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -20,5 +23,13 @@ public class DocController {
         List<Doc> docs = docStorageService.getFiles();
         model.addAttribute("docs", docs);
         return "doc";
+    }
+
+    @PostMapping("/uploadFiles")
+    public String uploadMultipleFiles(@RequestParam("files") MultipartFile [] files){
+        for(MultipartFile file: files){
+            docStorageService.saveFile(file);
+        }
+        return "redirect:/";
     }
 }
