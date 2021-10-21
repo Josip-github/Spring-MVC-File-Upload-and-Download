f1c1aaee602381492845a072140f37dfec02c8ca
Josip-github <josipcota1@gmail.com>
2021-10-21 10:39:41 +0200

    Initial commit Maven dependencies: Spring Web, DevTools, Thymeleaf, Spring Data JPA, H2 Database

---
+++ src/test/java/com/example/uploadanddownloadfilesapp/UploadAndDownloadFilesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadanddownloadfilesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadAndDownloadFilesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
