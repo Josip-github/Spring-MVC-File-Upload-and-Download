396b1693870cabd0ef5d9a5cdbf9b20ea88fba16
Josip-github <josipcota1@gmail.com>
2021-10-21 15:04:20 +0200

    This commit creates a method downloadFile()

--- src/main/java/com/example/uploadanddownloadfilesapp/controller/DocController.java
+++ src/main/java/com/example/uploadanddownloadfilesapp/controller/DocController.java
@@ -3,9 +3,14 @@ package com.example.uploadanddownloadfilesapp.controller;
 import com.example.uploadanddownloadfilesapp.model.Doc;
 import com.example.uploadanddownloadfilesapp.service.DocStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -31,5 +36,14 @@ public class DocController {
             docStorageService.saveFile(file);
         }
         return "redirect:/";
+    }
+
+    @GetMapping("/downloadFile/{fileId}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable Integer fileId){
+        Doc doc = docStorageService.getFile(fileId).get();
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(doc.getDocType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment:filename=\"" + doc.getDocName() + "\"")
+                .body(new ByteArrayResource(doc.getData()));
     }
 }
